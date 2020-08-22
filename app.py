7231f441eec3d61ced2ed8e18bd477d0ea01957a
Irham Mustofa Kamil <irhammusthofa@gmail.com>
2020-08-22 09:03:56 +0700

    try catch

--- app.py
+++ app.py
@@ -8,9 +8,12 @@ from requests.auth import HTTPBasicAuth
 import json
 
 def update(arr):
-    url = 'https://wa.golekonline.com/api1/message/outbox'
-    myobj = {'id': json.dumps(arr)}
-    x = requests.patch(url, data = myobj,auth=HTTPBasicAuth('what-bot','f0ba1f94ba5614a6c63357a5770a8e2707aab72e86e0ead124f769c7a7b5a1b0'))
+    try:
+        url = 'https://wa.golekonline.com/api1/message/outbox'
+        myobj = {'id': json.dumps(arr)}
+        x = requests.patch(url, data = myobj,auth=HTTPBasicAuth('what-bot','f0ba1f94ba5614a6c63357a5770a8e2707aab72e86e0ead124f769c7a7b5a1b0'))
+    except:
+        print('Error...')
 
 def init():
     pathdir = Path(__file__).parent.absolute()
@@ -26,25 +29,28 @@ def init():
     wabot_loop(driver)
 
 def wabot(driver):
-    status_driver = driver.get_status()
-    if (status_driver==WhatsAPIDriverStatus.LoggedIn):
-        url = "https://wa.golekonline.com/api1/message/outbox"
-        myResponse = requests.get(url,auth=HTTPBasicAuth('what-bot','f0ba1f94ba5614a6c63357a5770a8e2707aab72e86e0ead124f769c7a7b5a1b0'), verify=True)
-        # For successful API call, response code will be 200 (OK)
-        arr_id = []
-        if(myResponse.ok):
-            jData = json.loads(myResponse.content.decode('utf-8'))
-            for key in jData:
-                id = key['ow_id']
-                to = key['ow_to']
-                msg = key['ow_message']
-                arr_id.append(id)
-                print("Send Message '" + str(msg) +"' to " + str(to))
-                send_msg = driver.send_message_to_id(str(to)+"@c.us",str(msg))
-            
-            update(arr_id)
-        else:
-            myResponse.raise_for_status()
+    try:
+        status_driver = driver.get_status()
+        if (status_driver==WhatsAPIDriverStatus.LoggedIn):
+            url = "https://wa.golekonline.com/api1/message/outbox"
+            myResponse = requests.get(url,auth=HTTPBasicAuth('what-bot','f0ba1f94ba5614a6c63357a5770a8e2707aab72e86e0ead124f769c7a7b5a1b0'), verify=True)
+            # For successful API call, response code will be 200 (OK)
+            arr_id = []
+            if(myResponse.ok):
+                jData = json.loads(myResponse.content.decode('utf-8'))
+                for key in jData:
+                    id = key['ow_id']
+                    to = key['ow_to']
+                    msg = key['ow_message']
+                    arr_id.append(id)
+                    print("Send Message '" + str(msg) +"' to " + str(to))
+                    send_msg = driver.send_message_to_id(str(to)+"@c.us",str(msg))
+                
+                update(arr_id)
+            else:
+                myResponse.raise_for_status()
+    except:
+        print("Error..")
 
 def wabot_loop(driver):
     while True:
